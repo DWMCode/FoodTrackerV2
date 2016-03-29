8c4af4895fd9bcec044c7f4e70ba25c0cb0ae4b9
DWMCode <dwm.code@gmail.com>
2016-03-29 01:02:05 -0400

    Assignment 2 Commit

--- app/src/main/java/com/riansousa/foodtrackerv2/FoodListActivity.java
+++ app/src/main/java/com/riansousa/foodtrackerv2/FoodListActivity.java
@@ -12,10 +12,22 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+
+/**
+ * Class that handles all the code processing to support the activity_food_list view.
+ */
 public class FoodListActivity extends AppCompatActivity {
 
+    /**
+     * private global variables
+     */
     private static final String TAG = "FoodTracker";
 
+    /**
+     * Default method to load the activity
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +35,13 @@ public class FoodListActivity extends AppCompatActivity {
         initMain();
     }
 
+    /**
+     * Method to create the menu.
+     * Adapted from: Lecture Notes Module 1: Example illustrating state changes
+     *
+     * @param menu  Takes a Menu object
+     * @return      boolean value from super
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater mif = getMenuInflater();
@@ -31,97 +50,154 @@ public class FoodListActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    /**
+     * Method to handle option selection event.
+     * Launches a new Activity Screen based on selection.
+     * Adapted from: Lecture Notes Module 1: Example illustrating state changes
+     * research on switching between activity screens from
+     * http://stackoverflow.com/questions/17743094/how-to-switch-between-activities-screens-in-android
+     *
+     * @param item  The menu item selected by the user
+     * @return      boolean value from super
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // research on switching between activity screens from
+        // http://stackoverflow.com/questions/17743094/how-to-switch-between-activities-screens-in-android
         switch(item.getItemId())
         {
-            case R.id.wrench_icon:
+            case R.id.menu_food_list:
                 Log.i(TAG, "FoodListActivity - The food list icon was clicked");
                 // load food list screen
                 Intent foodListScreen = new Intent(getApplicationContext(), FoodListActivity.class);
                 startActivity(foodListScreen);
                 break;
-            case R.id.home_icon:
+            case R.id.menu_recorder:
                 Log.i(TAG, "FoodListActivity - The recorder icon was clicked");
                 // load food list screen
-                Intent recorder = new Intent(getApplicationContext(), RecorderActivity.class);
+                Intent recorder = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(recorder);
+                break;
+            case R.id.menu_alert:
+                Log.i(TAG, "FoodListActivity - The alert icon was clicked");
+                // load food list screen
+                Intent alert = new Intent(getApplicationContext(), AlertActivity.class);
+                startActivity(alert);
+                break;
+            case R.id.menu_reporting:
+                Log.i(TAG, "FoodListActivity - The reporting icon was clicked");
+                // load food list screen
+                Intent report = new Intent(getApplicationContext(), ReportingActivity.class);
+                startActivity(report);
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * System method used for logging activity life cycle onStart()
+     */
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "FoodListActivity - onStart");
     }
 
+    /**
+     * System method used for logging activity life cycle onResume()
+     */
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(TAG, "FoodListActivity - onResume");
     }
 
+    /**
+     * System method used for logging activity life cycle onPause()
+     */
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(TAG, "FoodListActivity - onPause");
     }
 
+    /**
+     * System method used for logging activity life cycle onStop()
+     */
     @Override
     protected void onStop() {
         super.onStop();
         Log.i(TAG, "FoodListActivity - onStop");
     }
 
+    /**
+     * System method used for logging activity life cycle onRestart()
+     */
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.i(TAG, "FoodListActivity - onRestart");
     }
 
+    /**
+     * System method used for logging activity life cycle onDestroy()
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "FoodListActivity - onDestroy");
     }
 
+    /**
+     * System method used for saving state instance
+     */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.i(TAG, "FoodListActivity - onSaveInstanceState");
     }
 
+    /**
+     * System method used for restoring state instance
+     */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         Log.i(TAG, "FoodListActivity - onRestoreInstanceState");
     }
 
+    /**
+     * Private method to handle the initial load of the activity
+     * used for setting default values and event listeners
+     * research on listview from http://www.tutorialspoint.com/android/android_list_view.htm
+     */
     private void initMain() {
         try {
-            // Instantiate listview object
-            ListView listview = (ListView) findViewById(R.id.listFoodProfile);
+            // Instantiate listview objects
+            ListView listNew = (ListView) findViewById(R.id.listNewItems);
+            ListView listExisting = (ListView) findViewById(R.id.listExistingItems);
 
             // Create a string array for seed data
             // todo: replace with data pull from repository
-            String[] values = new String[]{"Orange Juice - NEW", "Raisin Bran", "Fruit & Nuts Cup", "Coffee",
+            String[] valuesNew = new String[]{"Orange Juice"};
+            String[] valuesExisting = new String[]{"Raisin Bran", "Fruit & Nuts Cup", "Coffee",
                     "Banana", "Hot Dogs", "Chili", "Meatball Sub", "Pizza",
                     "Steak & Cheese", "Apple Juice", "Chicken Stir Fry", "Hamburger with Cheese", "Sloppy Joe", "Sword Fish",
                     "Sushi Regular"};
 
             // Create an adapter as the source for the list view. Note: MUST create TEXTVIEW layout for rendering
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.food_item, values);
+            ArrayAdapter<String> adapterNew = new ArrayAdapter<String>(this, R.layout.food_item, valuesNew);
+            ArrayAdapter<String> adapterExisting = new ArrayAdapter<String>(this, R.layout.food_item, valuesExisting);
 
             // Set the adapter as the list source
-            listview.setAdapter(adapter);
+            listNew.setAdapter(adapterNew);
+            listExisting.setAdapter(adapterExisting);
 
             // Log action
             Log.i(TAG, "FoodListActivity - Food Profile load complete");
 
-            // Create listener to capture list view item click
-            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // Create 1st listener to capture item click on NEW list
+            listNew.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
                 // Implement click handle
                 @Override
@@ -133,6 +209,33 @@ public class FoodListActivity extends AppCompatActivity {
                     Log.i(TAG, "FoodListActivity - You clicked " + item + " from food list.");
 
                     // create intent
+                    // research on intents and passing data between screens from
+                    // http://stackoverflow.com/questions/2091465/how-do-i-pass-data-between-activities-on-android
+                    Intent foodListDetailScreen = new Intent(getApplicationContext(), FoodListDetailActivity.class);
+
+                    // set key/value data pair to pass selected item
+                    foodListDetailScreen.putExtra("itemSelected", item);
+
+                    // load food list detail screen
+                    startActivity(foodListDetailScreen);
+                }
+            });
+
+            // Create 2nd listener to capture item click on EXISTING list
+            listExisting.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                // Implement click handle
+                @Override
+                public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                    // get item from position
+                    String item = (String) parent.getItemAtPosition(position);
+
+                    // Log item click
+                    Log.i(TAG, "FoodListActivity - You clicked " + item + " from food list.");
+
+                    // create intent
+                    // research on intents and passing data between screens from
+                    // http://stackoverflow.com/questions/17743094/how-to-switch-between-activities-screens-in-android
                     Intent foodListDetailScreen = new Intent(getApplicationContext(), FoodListDetailActivity.class);
 
                     // set key/value data pair to pass selected item
--- app/src/main/java/com/riansousa/foodtrackerv2/MainActivity.java
+++ app/src/main/java/com/riansousa/foodtrackerv2/MainActivity.java
@@ -15,100 +15,171 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-
+import android.widget.Toast;
 import com.riansousa.foodtrackerv2.Logic.Recorder;
 
-public class RecorderActivity extends AppCompatActivity {
+/**
+ * Class that handles all the code processing to support the activity_main view.
+ */
+public class MainActivity extends AppCompatActivity {
 
+    /**
+     * private global variables
+     */
     private static final String TAG = "FoodTracker";
 
+    /**
+     * Default method to load the activity
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recorder);
+        setContentView(R.layout.activity_main);
         initMain();
     }
 
+    /**
+     * Method to create the menu.
+     * Adapted from: Lecture Notes Module 1: Example illustrating state changes
+     *
+     * @param menu  Takes a Menu object
+     * @return      boolean value from super
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater mif = getMenuInflater();
         mif.inflate(R.menu.menu_main, menu);
-        Log.i(TAG, "RecorderActivity - Menu Options Loaded");
+        Log.i(TAG, "MainActivity - Menu Options Loaded");
         return super.onCreateOptionsMenu(menu);
     }
 
+    /**
+     * Method to handle option selection event.
+     * Launches a new Activity Screen based on selection.
+     * Adapted from: Lecture Notes Module 1: Example illustrating state changes
+     * research on switching between activity screens from
+     * http://stackoverflow.com/questions/17743094/how-to-switch-between-activities-screens-in-android
+     *
+     * @param item  The menu item selected by the user
+     * @return      boolean value from super
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // research on switching between activity screens from
+        // http://stackoverflow.com/questions/17743094/how-to-switch-between-activities-screens-in-android
         switch(item.getItemId())
         {
-            case R.id.wrench_icon:
-                Log.i(TAG, "RecorderActivity - The food list icon was clicked");
+            case R.id.menu_food_list:
+                Log.i(TAG, "MainActivity - The food list icon was clicked");
                 // load food list screen
                 Intent foodListScreen = new Intent(getApplicationContext(), FoodListActivity.class);
                 startActivity(foodListScreen);
                 break;
-            case R.id.home_icon:
-                Log.i(TAG, "RecorderActivity - The recorder icon was clicked");
+            case R.id.menu_recorder:
+                Log.i(TAG, "MainActivity - The recorder icon was clicked");
                 // load food list screen
-                Intent recorder = new Intent(getApplicationContext(), RecorderActivity.class);
+                Intent recorder = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(recorder);
+                break;
+            case R.id.menu_alert:
+                Log.i(TAG, "MainActivity - The alert icon was clicked");
+                // load food list screen
+                Intent alert = new Intent(getApplicationContext(), AlertActivity.class);
+                startActivity(alert);
+                break;
+            case R.id.menu_reporting:
+                Log.i(TAG, "MainActivity - The reporting icon was clicked");
+                // load food list screen
+                Intent report = new Intent(getApplicationContext(), ReportingActivity.class);
+                startActivity(report);
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * System method used for logging activity life cycle onStart()
+     */
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "RecorderActivity - onStart");
+        Log.i(TAG, "MainActivity - onStart");
     }
 
+    /**
+     * System method used for logging activity life cycle onResume()
+     */
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG, "RecorderActivity - onResume");
+        Log.i(TAG, "MainActivity - onResume");
     }
 
+    /**
+     * System method used for logging activity life cycle onPause()
+     */
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG, "RecorderActivity - onPause");
+        Log.i(TAG, "MainActivity - onPause");
     }
 
+    /**
+     * System method used for logging activity life cycle onStop()
+     */
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG, "RecorderActivity - onStop");
+        Log.i(TAG, "MainActivity - onStop");
     }
 
+    /**
+     * System method used for logging activity life cycle onRestart()
+     */
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i(TAG, "RecorderActivity - onRestart");
+        Log.i(TAG, "MainActivity - onRestart");
     }
 
+    /**
+     * System method used for logging activity life cycle onDestroy()
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG, "RecorderActivity - onDestroy");
+        Log.i(TAG, "MainActivity - onDestroy");
     }
 
+    /**
+     * System method used for saving state instance
+     */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        Log.i(TAG, "RecorderActivity - onSaveInstanceState");
+        Log.i(TAG, "MainActivity - onSaveInstanceState");
     }
 
+    /**
+     * System method used for restoring state instance
+     */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        Log.i(TAG, "RecorderActivity - onRestoreInstanceState");
+        Log.i(TAG, "MainActivity - onRestoreInstanceState");
     }
 
+    /**
+     * Private method to handle the initial load of the activity
+     * used for setting default values and event listeners
+     * research on listview from http://www.tutorialspoint.com/android/android_list_view.htm
+     */
     private void initMain() {
 
         // Instantiate listview object
-        ListView listview = (ListView) findViewById(R.id.listFood);
+        ListView listview = (ListView) findViewById(R.id.listMainItems);
 
         // Create a string array for seed data
         // todo: replace with data pull from repository
@@ -124,7 +195,7 @@ public class RecorderActivity extends AppCompatActivity {
         listview.setAdapter(adapter);
 
         // Log action
-        Log.i(TAG, "RecorderActivity - Item load complete");
+        Log.i(TAG, "MainActivity - Item load complete");
 
         // Create listener to capture list view item click
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -136,31 +207,14 @@ public class RecorderActivity extends AppCompatActivity {
                 String item = (String) parent.getItemAtPosition(position);
 
                 // Log item click
-                Log.i(TAG, "RecorderActivity - You clicked " + item);
+                Log.i(TAG, "MainActivity - You clicked " + item);
 
                 // Send item to logic layer for processing
                 Recorder lr = new Recorder();
                 lr.RecordConsumption(item);
 
-                // Create alert
-                AlertDialog.Builder builder1 = new AlertDialog.Builder(RecorderActivity.this);
-
-                // Set properties
-                builder1.setMessage("Your consumption of " + item + " has been recorded.");
-                builder1.setCancelable(true);
-
-                // Add OK button
-                builder1.setNegativeButton("OK",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // close the dialog
-                                dialog.cancel();
-                            }
-                        });
-
-                // Pop confirm alert
-                AlertDialog alert11 = builder1.create();
-                alert11.show();
+                // send Toast confirm
+                Toast.makeText(getApplicationContext(), "Your consumption of " + item + " has been recorded.", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -172,23 +226,25 @@ public class RecorderActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // handle click
-                Log.i(TAG, "RecorderActivity - Add New Food Item Button Clicked");
+                Log.i(TAG, "MainActivity - Add New Food Item Button Clicked");
                 // launch pop up dialog for new item
                 showAddNewDialog();
             }
         });
     }
 
+    // code pattern on input dialog adapted from http://javatechig.com/android/android-input-dialog-example
+    // customization of activity class, layout views and click implementation.
     private void showAddNewDialog() {
 
         // instantiate layout inflator
-        LayoutInflater layoutInflater = LayoutInflater.from(RecorderActivity.this);
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
 
         // load the new_item input dialog view
         View newItemView  = layoutInflater.inflate(R.layout.new_item, null);
 
         // create modal with AlertDialog
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(RecorderActivity.this);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
         alertDialogBuilder.setView(newItemView);
 
         // get the edittext object from the new_item view
@@ -202,13 +258,19 @@ public class RecorderActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int id) {
                         try {
                             // get the item input
+                            // research on "getting" UI data from
+                            // http://stackoverflow.com/questions/4531396/get-value-of-a-edit-text-field
                             android.text.Editable item = editText.getText();
-                            Log.i(TAG, "RecorderActivity - " + editText.getText() + " was entered as a new item.");
+                            Log.i(TAG, "MainActivity - " + editText.getText() + " was entered as a new item.");
                             // send item to logic layer for processing
                             Recorder lr = new Recorder();
                             lr.AddNewItem(item);
+
+                            // send Toast confirm
+                            Toast.makeText(getApplicationContext(), item + " has been added.", Toast.LENGTH_SHORT).show();
+
                         } catch (Exception e) {
-                            Log.i(TAG, "RecorderActivity ERROR - " + e.getMessage());
+                            Log.i(TAG, "MainActivity ERROR - " + e.getMessage());
                         }
                     }
                 })
